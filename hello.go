5a4a648601cbe10e7e5945ffe31186aba48536e7
Johan Wennerberg <j.wennerberg@gmail.com>
2021-04-13 10:24:51 +0200

    hello with metrics

--- hello.go
+++ hello.go
@@ -1,23 +1,25 @@
 package main
 
 import (
+  "fmt"
   "log"
   "net/http"
   "github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func ApiHandler(w http.ResponseWriter, r *http.Request) {
-  w.Write([]byte("go api testing\n"))
+  fmt.Fprintf(w, "go api testing", r.URL.Path[1:])
 }
 
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
-  w.Write([]byte("Hi there!\n"))
+  fmt.Fprintf(w, "Hi there!")
 }
 
 func main() {
   http.Handle("/metrics", promhttp.Handler())
+  http.HandleFunc("/", HelloHandler)
   http.HandleFunc("/hello", HelloHandler)
   http.HandleFunc("/api", ApiHandler)
 
-  log.Fatal(http.ListenAndServe(":8000", r))
+  log.Fatal(http.ListenAndServe(":8080", nil))
 }
